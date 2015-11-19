114155b68d4d62248885d8675d260edebfae9215
AlCue <alex.cue.so.92@gmail.com>
2015-11-19 17:36:28 +0100

    primer comentario

---
+++ app/src/main/java/com/example/alex/projectsunshine/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alex.projectsunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// AQIIIIIIIIIIIIIIIIIIIII
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
